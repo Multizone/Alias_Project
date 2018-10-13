1828037b249b32838fc637840939de9a6eaf1e83
Multizone <Multizone>
2018-10-13 21:16:20 +0300

    second commit

--- app/src/main/java/aliasproject/com/alias/MainActivity.java
+++ app/src/main/java/aliasproject/com/alias/MainActivity.java
@@ -1,7 +1,10 @@
 package aliasproject.com.alias;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRules(View view) {
+        Intent rules = new Intent(MainActivity.this, Rules.class);
+        startActivity(rules);
     }
 }
